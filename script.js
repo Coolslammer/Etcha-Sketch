be0ed783fed8637691619519dfb784b6869c4725
Samuel Boehle <coolslammer@gmail.com>
2016-12-09 22:15:15 -0500

    Done

--- script.js
+++ script.js
@@ -1,12 +1,39 @@
-var makegird = function(){
-	$("#holder").append("<p><div class=box></div></p>");
-
-}
-
-
 $(document).ready(function(){
-	for(i = 0; i < 16; i++) {
-		makegird;
-	}
+	var numOfSqaures = 16;
+	var count = 0;
+	
+	$("body").append("<button type='button' id='button'>Reset</button>");
+	$("body").append("<div id='container'></div>");
 
+	function display() {
+		while (count < numOfSqaures) {
+			count += 1;
+			$("#container").append("<div class='screen' id='row" + count +"'></div>");
+			var count1 = 0;
+			while (count1 < numOfSqaures) {
+				$("#row"+count).append("<div class='screen'></div>");
+				count1 += 1;
+			}
+		}
+	}
+	display();
+
+	function shader() {
+		$(".screen").hover(function(){
+			$(this).css("background-color", "red");
+		})
+	}
+	shader();
+	
+	$(document).on("click", "button", function screenReset() {
+		numOfSqaures = prompt("Size of new Screen");
+		$("#container").empty();
+		$(".screen").css("background-color", "grey");
+		display();
+		count = 0;
+		var dimension = 500/numOfSqaures;
+		$(".screen").css("height", dimension);
+		$(".screen").css("width", dimension);
+		shader();
+	})
 })
